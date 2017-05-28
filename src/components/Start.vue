6c38a8f75fd2d6d0b63febd862aba8a4d7c9c100
baback <baback@Babacks-MacBook-Pro.local>
2017-05-28 14:48:59 +0430

    index first

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -1,12 +1,44 @@
 <template>
-  <div>
-  
-  starter
+    <div class="container">
 
-  </div>
+      <section class="cover-container">
+      </section> <!-- End cover-container -->
+
+      <section class="form-container">
+        <form class="form-url" method="post">
+          <input type="text" name="url" placeholder="http://example.com/single-article" value="">
+          <input type="submit" value="Start">
+        </form>
+        <span class="form-hint">Enter a URL to create template</span>
+      </section> <!-- End form-container -->
+
+      <footer>
+        <p>footer</p>
+      </footer>
+
+    </div> <!-- container -->
 </template>
 
 <style lang="css">
+  .container{
+    width : 1170px;
+    margin: 0 auto;
+  }
+  .cover-container{
+    background-color : #cecece;
+    width: 100%;
+    height: 300px;
+  }
+  .form-container{
+  }
+  .form-url{
+  }
+  .form-url input[type='text']{
+  }
+  .form-url input[type='submit']{
+  }
+  .form-hint{
+  }
 
 </style>
 

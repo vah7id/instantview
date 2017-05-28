33806b9790c72f1e0b005e9bce7b6f125ac2b71e
baback <baback@Babacks-MacBook-Pro.local>
2017-05-28 15:24:25 +0430

    index update

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -2,6 +2,7 @@
     <div class="container">
 
       <section class="cover-container">
+        <img src="src/assets/img/instant.jpeg">
       </section> <!-- End cover-container -->
 
       <section class="form-container">
@@ -20,24 +21,46 @@
 </template>
 
 <style lang="css">
+  body{
+    font-family: 'Lato', sans-serif;
+  }
   .container{
-    width : 1170px;
+    width: 1170px;
     margin: 0 auto;
   }
   .cover-container{
-    background-color : #cecece;
-    width: 100%;
-    height: 300px;
+    text-align: center;
+  }
+  .cover-container > img{
+    width: 600px;
   }
   .form-container{
+    width: 60%;
+    margin : 0 auto;
+    text-align: center;
   }
   .form-url{
   }
   .form-url input[type='text']{
+    height: 42px;
+    padding: 10px 16px;
+    font-size: 15px;
+    line-height: 1.33;
+    width : calc(100% - 132px);
+    box-sizing: border-box;
+    outline: none;
+    box-shadow: none;
   }
   .form-url input[type='submit']{
+    padding: 10px 16px;
+    font-size: 15px;
+    line-height: 1.33;
+    width : 70px;
   }
   .form-hint{
+  }
+  footer{
+    text-align: center;
   }
 
 </style>

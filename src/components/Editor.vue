625513bfa93b5cead814e733348c85640c179c58
baback <baback@Babacks-MacBook-Pro.local>
2017-05-28 16:52:52 +0430

    styles update

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,12 +1,61 @@
 <template>
   <div>
-  
-  editor
+    <section class="editor">
+        <div class="url-parse-container">
 
+        </div>
+    </section>
+    <aside>
+      <div class="aside-title">
+        <h3>Checklist</h3>
+        <ul>
+          <li><span>One</span></li>
+          <li><span>Two</span></li>
+          <li><span>Three</span></li>
+          <li><span>Four</span></li>
+          <li><span>Five</span></li>
+          <li><span>Six</span></li>
+          <li><span>Seven</span></li>
+          <li><span>Eight</span></li>
+          <li><span>Nine</span></li>
+          <li><span>Ten</span></li>
+        </ul>
+        <div class="actions-container">
+          <ul>
+            <li><span>Show Preview</span></li>
+            <li><span>Create Template</span></li>
+          </ul>
+        </div>
+      </div>
+    </aside>
   </div>
 </template>
 
 <style lang="css">
+
+  /* --- EDITOR STYLES --- */
+
+  .editor{
+    width : calc(100% - 340px);
+    height : 100%;
+    box-sizing : border-box;
+  }
+  .url-parse-container{
+    background-color : #efefef;
+    height : calc(100% - 0px);
+  }
+  aside{
+    width : 300px;
+    padding : 20px;
+    position : fixed;
+    right:0;
+    top:0;
+    bottom:0;
+  }
+  .actions-container{
+    position : absolute;
+    bottom:0;
+  }
 
 </style>
 

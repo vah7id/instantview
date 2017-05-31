6a4c7e9fd43db99bbada39a41246287062c99075
Vahid Taghizadeh <vah7id@gmail.com>
2017-05-31 18:39:02 +0430

    panel copy clipboard

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -5,8 +5,8 @@
       <div class="container">
         <div v-for="item in items" class="domain-item">
             <h3>{{ item.domain }}</h3>
-            <p>Sample URI : <a :href="item.url">{{ item.url }} </a></p>
             <button v-on:click="alert( item.template )">GET TEMPLATE CONTENT</button>
+            <p>Sample URI : <a :href="item.url">{{ item.url }} </a></p>
             <small>Generated at : {{ item.created_at }}</small>
         </div>
       </div>
@@ -17,10 +17,11 @@
 <style lang="css">
 .panel-karim h2{margin-top: 0;background: #222 !important;color:#fff;font-weight:bold !important;padding: 20px 65px;text-transform: uppercase;}
 .domain-item{
-  width: 25%;
+  width: 100%;
+  margin: 20px 0;
   float: left;
+  position: relative;
   background-color: #eee;
-  border: 10px solid #fff;
 }
 .domain-item small{
   opacity: 0.5;
@@ -29,16 +30,18 @@
   float: left;
 }
 .domain-item h3{
+  font-weight: bold;
   padding: 5px 15px;
 }
 .domain-item button{
-  margin-bottom: 10px;
-  margin-left: 14px;
+  margin-top:-40px;
+  margin-right: 20px;
+  float: right;
 }
 .domain-item p{
-  width: 85%;
-  margin: 20px auto;
-  display: block;
+  width: 90%;
+  margin: 10px 15px 5px 15px;
+  float: left;
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
@@ -49,6 +52,7 @@
 
   import store from '../store';
   const request = require("browser-request");
+  const clipboard = require("clipboard-js");
 
   export default {
 
@@ -78,8 +82,36 @@
               }
             });
         },
+        copyToClipboard(text) {
+            if (window.clipboardData && window.clipboardData.setData) {
+                // IE specific code path to prevent textarea being shown while dialog is visible.
+                return clipboardData.setData("Text", text); 
+
+            } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+                var textarea = document.createElement("textarea");
+                textarea.textContent = text;
+                textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
+                document.body.appendChild(textarea);
+                textarea.select();
+                try {
+                    return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+                } catch (ex) {
+                    console.warn("Copy to clipboard failed.", ex);
+                    return false;
+                } finally {
+                    document.body.removeChild(textarea);
+                }
+            }
+        },
         alert(txt){
-          alert(txt)
+          alert(txt);
+          clipboard.copy({
+            'text/plain': txt,
+            'text/html': txt
+          }).then(
+            function(){console.log('success'); },
+            function(err){console.log('failure', err);
+          });
         }
       }
   }

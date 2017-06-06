f5cb57f6f653b9a62f9e76fd4da5479d17b6a059
Vahid Taghizadeh <vah7id@gmail.com>
2017-06-06 16:43:32 +0430

    remove elements fixed

--- build/build.js
+++ build/build.js
@@ -1,13 +1,13 @@
 // https://github.com/shelljs/shelljs
 require('./check-versions')()
 require('shelljs/global')
-env.NODE_ENV = 'production'
+env.NODE_ENV = 'development'
 
 var path = require('path')
 var config = require('../config')
 var ora = require('ora')
 var webpack = require('webpack')
-var webpackConfig = require('./webpack.prod.conf')
+var webpackConfig = require('./webpack.dev.conf')
 
 console.log(
   '  Tip:\n' +
@@ -21,7 +21,7 @@ spinner.start()
 var assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirectory)
 rm('-rf', assetsPath)
 mkdir('-p', assetsPath)
-cp('-R', 'static/*', assetsPath)
+cp('-R', 'static', assetsPath)
 
 webpack(webpackConfig, function (err, stats) {
   spinner.stop()
